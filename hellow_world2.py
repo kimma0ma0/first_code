474d8dffa9a1e45285e527fc07f96a57f95108d3
kimma0ma0 <a01029683372@gmail.com>
2024-03-07 17:27:57 +0900

    third commit

--- hellow_world2.py
+++ hellow_world2.py
@@ -1,3 +1,26 @@
 print("hello World!")
 print("hello World!")
 print("hello World!")
+
+print('Hello', 'world!')
+print('Hello'+'world!')
+print(100)
+print(2+4)
+print(100)
+print(10+20)
+print(10-20)
+print(10*20)
+print(10/20)
+print(123*8765)
+print(10+20*30)
+
+x=100
+y=200
+print(x+y)
+print(x-y)
+print(x*y)
+print(x/y)
+print(x**2)
+n=5
+print(n**2)
+
