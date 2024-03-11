95b984d564048103cdfb148247729ca65909bf04
kimma0ma0 <a01029683372@gmail.com>
2024-03-11 15:49:01 +0900

    third commit

---
+++ 나이가 너무 많아요.py
@@ -0,0 +1,12 @@
+Python 3.12.2 (tags/v3.12.2:6abddd9, Feb  6 2024, 21:26:36) [MSC v.1937 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print(0.1+0.1==0.2)
+True
+>>> print(0.1+0.1+0.1==0.2)
+False
+>>> age=input('몇살이세용?')
+몇살이세용?
+>>> if age<20:
+...     print('청소년 할인 대상입니다.')
+... else:
+...     print('나이가 너무 많아버리세요;;^^')
