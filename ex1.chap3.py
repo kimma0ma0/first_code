2f414844d5841c2798a597826a835bf1b21b829b
kimma0ma0 <hayoung17188@gmail.com>
2024-03-19 16:05:34 +0900

    third commit

---
+++ ex1.chap3.py
@@ -0,0 +1,16 @@
+100>200
+100>=200
+100<200
+100<=200
+100==200
+100!=200
+200==200
+200!=200
+True or True
+True or False
+True and False
+True and True
+True or True and False
+True and True or False
+'Morning'< 'morning'
+'A'>'B'
