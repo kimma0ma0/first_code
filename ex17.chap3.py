4badd01b51cf3958d930c5de6dec28cd39a14879
kimma0ma0 <hayoung17188@gmail.com>
2024-03-20 22:49:26 +0900

    third commit

---
+++ ex17.chap3.py
@@ -0,0 +1,16 @@
+ #1
+for i in range(3):
+    print('python ')
+    print('is ')
+    print('FUN! ')
+#2
+for i in range(3):
+    print('python ')
+    print('is ')
+print('fun!')
+
+#3
+for i in range(3):
+    print('python')
+print('is ')
+print('fun!')
