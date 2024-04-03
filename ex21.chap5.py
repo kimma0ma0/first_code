a86340a554aaa21c5822cf8716822b571138f10c
kimma0ma0 <hayoung17188@gmail.com>
2024-04-03 21:35:55 +0900

    third commit

---
+++ ex21.chap5.py
@@ -0,0 +1,17 @@
+src ='a2b3c6a2c3f1g1'
+print('src=',src)
+
+for ch in src:
+     if ch.isnumeric():
+         for i in range(int(ch)):
+             
+             print(ch_old, end='')
+     else:
+         ch_old =ch
+
+print('output=',ch)
+
+#''도 안붙고 출력 이상하게 됨.질문이요.
+
+
+
